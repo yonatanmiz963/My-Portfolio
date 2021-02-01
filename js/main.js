669751ae94b25bcf2d0eaf806b7b0442377ffe4b
Yonatan Mizrahi <yoni9696@walla.com>
2021-02-01 18:13:48 +0200

    Second Commit, created a render modal function, rende portfolio and a model for them

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
-console.log('Starting up');
+'use strict';
+
+var gProjs;
+
+$('.portfolio-hover').on('click', function() {
+    renderModal(this);
+});
+
+
+
+$(document).ready(function initPage() {
+    console.log('Starting up');
+    gProjs = generateProjs();
+    renderProjects();
+
+    // console.log('proj 0 por:', gProjs[0].por);
+    // new Date(stamp).toDateString()
+});
+
+function renderProjects() {
+    gProjs.forEach(function(proj) {
+        renderPortfolio(proj);
+    })
+}
+
+
+
+function renderPortfolio(proj) {
+    var $portfolio = $(`.por-${proj.por}`);
+    $portfolio.find('h4').text(`${proj.name}`);
+    $portfolio.find('p').text(`${proj.labels}`);
+    // console.log($portfolio);
+}
+
+
+function renderModal(elPortfolio) {
+    console.log(elPortfolio);
+    var $elPortfolio = $(elPortfolio);
+    // console.log($elPortfolio);
+    var id = $elPortfolio.attr("data-id");
+    // console.log(id);
+    var currProj = gProjs[id];
+    var $modal = $('.modal');
+    $modal.find('h2').text(currProj.name);
+    $modal.find('.item-intro').text(currProj.title);
+    $modal.find('.desc').text(currProj.desc);
+    $modal.find('.date').text(`Created at: ${new Date(currProj.publishedAt).toDateString()}`);
+    $modal.find('.label').text(`Category: ${currProj.labels}`);
+    $modal.find('.link').click(function() {
+        window.open(`${currProj.url}`);
+    });
+}
---
+++ js/proj-service.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+var gProjs;
+
+
+function generateProjs() {
+    return [{
+            por: 0,
+            id: "mine-sleeper",
+            name: "Mine Sleeper",
+            title: "Find all mines and win!",
+            desc: "Coding this game was my first sprint in the Coding Academy course, it was an enriching and challenging experience, in this game your goal is to mark all the mine squares with a flag then you win. ",
+            url: "https://yonatanmiz963.github.io/Sprint-1/",
+            publishedAt: 1611439200000,
+            labels: "Matrixes",
+        },
+        {
+            por: 1,
+            id: "book-shop",
+            name: "Book Shop",
+            title: "a bookshop data storage",
+            desc: "In this project I had learn about the MVC and CRUDL concepts which stand for dividing the roles of each file, the view, the controller and the model. ",
+            url: "https://yonatanmiz963.github.io/book-shop/",
+            publishedAt: 1611784800000,
+            labels: "MVC, CRUDL"
+        },
+        {
+            por: 2,
+            id: "guess-who",
+            name: "Guess Who",
+            title: "A Smart Jinny",
+            desc: "In this project we have created a bot using binary trees and gets more knowledge each time you tell him new things, he guesses who you think of eventually. ",
+            url: "https://yonatanmiz963.github.io/Guess-Who/",
+            publishedAt: 1612044000000,
+            labels: "Binary Trees"
+        }
+
+    ]
+}
