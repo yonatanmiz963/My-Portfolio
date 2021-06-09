474929a2417d3a97a69427c3221d17859bf946f4
Yonatan Mizrahi <yonatanmiz963@gmail.com>
2021-06-09 22:59:46 +0300

    updated

--- js/proj-controller.js
+++ js/proj-controller.js
@@ -1,9 +1,6 @@
 'use strict';
 
 
-
-
-
 $(function initPage() {
     generateProjs();
     var projects = getProjects();
@@ -13,7 +10,7 @@ $(function initPage() {
 
 function renderPortfolios(projects) {
 
-    var portfoliosHTML = projects.map(function(proj) {
+    var portfoliosHTML = projects.map(function (proj) {
 
         return `<div class="col-md-4 col-sm-6 portfolio-item">
         <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
@@ -22,7 +19,7 @@ function renderPortfolios(projects) {
         <i class="fa fa-plus fa-3x"></i>
         </div>
         </div>
-        <img class="img-fluid" src="img/portfolio/${proj.id}.png" alt="">
+        <img class="img-fluid" src="img/portfolios/${proj.id}.png" alt="">
         </a>
         <div class="portfolio-caption">
         <h4>${proj.name}</h4>
@@ -46,9 +43,9 @@ function onRenderModal(id) {
     $modal.find('h2').text(project.name);
     $modal.find('.item-intro').text(project.title);
     $modal.find('.desc').text(project.desc);
-    $modal.find('.date').text(`Created at: ${new Date(project.publishedAt).toDateString()}`);
-    $modal.find('.label').text(`Category: ${project.labels}`);
-    $modal.find('.link').click(function() {
+    // $modal.find('.date').text(`Created at: ${new Date(project.publishedAt).toDateString()}`);
+    $modal.find('.label').text(`Technologics: ${project.techs}.`);
+    $modal.find('.link').click(function () {
         window.open(`${project.url}`);
     });
 }
--- js/proj-service.js
+++ js/proj-service.js
@@ -6,65 +6,93 @@ var gProjs;
 
 function generateProjs() {
     gProjs = [{
-            por: 0,
-            id: "minesleeper",
-            name: "Mine Sleeper",
-            title: "Find all mines and win!",
-            desc: "Coding this game was my first sprint in the Coding Academy course, it was an enriching and challenging experience, in this game your goal is to mark all the mine squares with a flag then you win. ",
-            url: "https://yonatanmiz963.github.io/Sprint-1/",
-            publishedAt: 1611439200000,
-            labels: "Matrixes",
-        },
-        {
-            por: 1,
-            id: "bookshop",
-            name: "Book Shop",
-            title: "a bookshop data storage",
-            desc: "In this project I had learn about the MVC and CRUDL concepts which stand for dividing the roles of each file, the view, the controller and the model. ",
-            url: "https://yonatanmiz963.github.io/book-shop/",
-            publishedAt: 1611784800000,
-            labels: "MVC, CRUDL"
-        },
-        {
-            por: 2,
-            id: "guesswho",
-            name: "Guess Who",
-            title: "A Smart Jinny",
-            desc: "In this project we have created a bot using binary trees and gets more knowledge each time you tell him new things, he guesses who you think of eventually. ",
-            url: "https://yonatanmiz963.github.io/Guess-Who/",
-            publishedAt: 1612044000000,
-            labels: "Binary Trees"
-        },
-        {
-            por: 3,
-            id: "pacman",
-            name: "Pacman",
-            title: "The first video game",
-            desc: "During this project we have created the legendary Pacman game, learning alot about logic, and the concept of update model and then rendering to DOM. ",
-            url: "https://yonatanmiz963.github.io/Pacman/",
-            publishedAt: 1611007200000,
-            labels: "Matrixes"
-        },
-        {
-            por: 4,
-            id: "ballboard",
-            name: "Ball-Board",
-            title: "Get all the balls!",
-            desc: "In this project I have learnt alot about logic, matrixes as models and rendering to DOM. ",
-            url: "https://yonatanmiz963.github.io/Ball-Board/",
-            publishedAt: 1610834400000,
-            labels: "Materixes"
-        },
-        {
-            por: 5,
-            id: "todo",
-            name: "To-Do",
-            title: "A useful to do list",
-            desc: "In this project I learnt how to use the MVC concept, also using array extras. ",
-            url: "https://yonatanmiz963.github.io/To-do-list/",
-            publishedAt: 1611698400000,
-            labels: "MVC"
-        }
+        por: 0,
+        id: "due",
+        name: "Due",
+        title: "Monday.com clone",
+        desc: "Monday.com alike application for projects management.",
+        url: "http://dueapp.herokuapp.com/",
+        labels: "Project Management Tool",
+        techs: "Vue, Vuex, Node.js, MongoDB, Web Sockets"
+    },
+    {
+        por: 1,
+        id: "appsus",
+        name: "Appsus",
+        title: "Notes and Mailbox",
+        desc: "Google keep alike and Mail application.",
+        url: "https://yonatanmiz963.github.io/Appsus/#/",
+        labels: "Notes and Mailbox",
+        techs: "Vue"
+    },
+    {
+        por: 1,
+        id: "bitcoin",
+        name: "Mister-Bitcoin",
+        title: "Coin Trade",
+        desc: "Bitcoin trade application, transfer coins to your contacts.",
+        url: "https://yonatanmiz963.github.io/bitcoin/#/signup",
+        labels: "Coin Trade",
+        techs: "React"
+    },
+    {
+        por: 1,
+        id: "designtolive",
+        name: "Design to live",
+        title: "Responsive Design",
+        desc: "A pixel perfect responsive design projcet.",
+        url: "https://yonatanmiz963.github.io/design-to-live/#",
+        labels: "Responsive Design",
+        techs: "CSS, HTML5"
+    },
+    {
+        por: 1,
+        id: "meme",
+        name: "MemeGen",
+        title: "Meme Generator",
+        desc: "A meme generator application allowing you to create custom memes, download them or share them with facebook.",
+        url: "https://yonatanmiz963.github.io/Meme-Generator/",
+        labels: "Meme Generator",
+        techs: "CSS, HTML5, Vanila, MVC"
+    },
+    {
+        por: 1,
+        id: "missbook",
+        name: "MissBook",
+        title: "Books Stock",
+        desc: "A book stock allowing you to explore new books, review them and even search and add new books to the stock.",
+        url: "https://yonatanmiz963.github.io/miss-book/#/",
+        labels: "Books Stock",
+        techs: "Vue"
+    },
+    {
+        por: 1,
+        id: "mistertoy",
+        name: "MisterToy",
+        title: "eCommerce",
+        desc: "A Toy shop eCommerce application with user authentication, allows the users to review the items they purchased, a live chat for each item updating in real time.",
+        url: "https://mr-toy-shop.herokuapp.com/#/",
+        labels: "eCommerce",
+        techs: "Vue, Vuex, Node.js, MongoDB, Web Sockets"
+    },
+    // {
+    //     por: 3,
+    //     id: "pacman",
+    //     name: "Pacman",
+    //     title: "The first video game",
+    //     desc: "During this project we have created the legendary Pacman game, learning alot about logic, and the concept of update model and then rendering to DOM. ",
+    //     url: "https://yonatanmiz963.github.io/Pacman/",
+    //     labels: "Matrixes"
+    // },
+    // {
+    //     por: 4,
+    //     id: "ballboard",
+    //     name: "Ball-Board",
+    //     title: "Get all the balls!",
+    //     desc: "In this project I have learnt alot about logic, matrixes as models and rendering to DOM. ",
+    //     url: "https://yonatanmiz963.github.io/Ball-Board/",
+    //     labels: "Materixes"
+    // },
     ]
 }
 
@@ -73,7 +101,7 @@ function getProjects() {
 }
 
 function getProjectById(id) {
-    var project = gProjs.find(function(proj) {
+    var project = gProjs.find(function (proj) {
         return proj.id === id;
     })
     return project;
